b41882107ea75fe0bbffa3465667439334bf98af
TarDim <53646594+DR-Dock@users.noreply.github.com>
2021-02-08 02:40:15 +0600

    Delete newspaper directory

--- newspaper/news/urls.py
+++
@@ -1,8 +0,0 @@
-from .views import *
-from django.urls import path
-urlpatterns = [
-    path('', news_list, name='news_list_url'),
-   	path('news/add/', NewsAdd.as_view(), name='news_add_url'),
-    path('news/<str:id>/', news_detail, name='news_detail_url'),
-
-]
